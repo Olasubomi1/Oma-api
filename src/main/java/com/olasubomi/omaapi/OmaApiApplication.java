cbdbe60fce1d799f3af9d94aa8a0594f6b4fe767
Olasubomi <shubbynlaa@gmail.com>
2023-02-06 02:27:08 +0100

    Initial commit.

---
+++ src/main/java/com/olasubomi/omaapi/OmaApiApplication.java
@@ -0,0 +1,13 @@
+package com.olasubomi.omaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olasubomi/omaapi/OmaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olasubomi.omaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
