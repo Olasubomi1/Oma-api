3fe1f6b6bf31522494a0d5886fc4f3904685ac05
Olasubomi <shubbynlaa@gmail.com>
2023-02-06 23:42:56 +0100

    Restructured project entrance file

--- src/main/java/com/olasubomi/omaapi/OmaApiApplication.java
+++ src/main/java/com/olasubomi/omaapi/OmaApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OmaApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OmaApiApplication.class, args);
 	}
-
 }
