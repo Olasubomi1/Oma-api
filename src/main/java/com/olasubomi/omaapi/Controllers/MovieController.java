d66f1feda2fe8fd80effff06c0c95eb7c2c892c7
Olasubomi <shubbynlaa@gmail.com>
2023-02-07 03:25:42 +0100

    Created a MovieController class.

--- src/main/java/com/olasubomi/omaapi/Controllers/MovieController.java
+++ src/main/java/com/olasubomi/omaapi/Controllers/MovieController.java
@@ -1,2 +1,16 @@
-package com.olasubomi.omaapi.Controllers;public class MovieController {
+package com.olasubomi.omaapi.Controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+
+    @GetMapping
+    public ResponseEntity<String> getAllMovies(){
+        return ResponseEntity.ok("List of all movies.");
+    }
 }
