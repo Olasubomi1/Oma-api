192fdd85c7087dd94c9f246c8f3f362e77ffc346
Olasubomi <shubbynlaa@gmail.com>
2023-02-08 02:42:37 +0100

    Create an api endpoint for getting movie by its Id.

--- src/main/java/com/olasubomi/omaapi/Services/IMovieService.java
+++ src/main/java/com/olasubomi/omaapi/Services/IMovieService.java
@@ -1,9 +1,12 @@
 package com.olasubomi.omaapi.Services;
 
 import com.olasubomi.omaapi.Model.Movie;
+import org.bson.types.ObjectId;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IMovieService {
     List<Movie> allMovies();
+    Optional<Movie> getMovieById(ObjectId id);
 }
--- src/main/java/com/olasubomi/omaapi/Services/MovieService.java
+++ src/main/java/com/olasubomi/omaapi/Services/MovieService.java
@@ -2,10 +2,12 @@ package com.olasubomi.omaapi.Services;
 
 import com.olasubomi.omaapi.Model.Movie;
 import com.olasubomi.omaapi.Repository.MovieRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService implements IMovieService{
@@ -17,4 +19,11 @@ public class MovieService implements IMovieService{
     public List<Movie> allMovies() {
         return movieRepository.findAll();
     }
+
+    @Override
+    public Optional<Movie> getMovieById(ObjectId id) {
+        return movieRepository.findById(id);
+    }
+
+
 }
